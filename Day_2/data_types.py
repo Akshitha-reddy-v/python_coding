15a3ad10442d38baab5c855ba7facbbe143ee5d1
Akshitha-reddy-v <142597246+Akshitha-reddy-v@users.noreply.github.com>
2025-05-14 18:06:41 +0530

    Create data_types.py

---
+++ Day_2/data_types.py
@@ -0,0 +1,23 @@
+# Python automatically detects variable type, making coding more efficient.
+""" format for initialization of a variable in Python:
+    variable_type = value """
+
+# numbers - int for whole numbers and float for real numbers.
+num1 = 5
+num2 = 4.9
+
+# string(str) - sequence of characters
+fruit = "Kiwi"
+color = 'Green'
+
+# char - used to store a single character
+letter = 'V'
+
+# boolean - used to store only 2 values (True-1 and False-0)
+isTrue = True    # python is case-sensitive, so true in place of True is not allowed
+isFalse = False
+
+# None - special variable that represents 'nothing' or 'no value'
+# None is like an empty box, it exists but there is nothing in it.
+name = None    # name hasn't been entered yet
+score = None
