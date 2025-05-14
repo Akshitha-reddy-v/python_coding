fdf424a8e46b6f6661cf8757332216f689de0bc3
Akshitha-reddy-v <142597246+Akshitha-reddy-v@users.noreply.github.com>
2025-05-14 18:32:05 +0530

    Create operators.py

---
+++ Day_3/operators.py
@@ -0,0 +1,28 @@
+# Operators - perform operations on the values.
+
+# Arithmetic Operators (+,-,*,/)
+a = 5
+b = 7.4
+c = a + b    # c holds 12.4
+d = a - b    # d holds -2.4
+e = a * b    # e holds 37.0
+f = a / b    # f holds 0.6756756756756757
+
+# Modulo Operator (%) - gives the remainder of a division.
+# usually used to know whether a number is even or odd
+g = 5
+f = 8
+h = g % 2  # h holds 1
+i = f % 3  # i holds 2
+
+# Arithmetic shortcuts (+=,-=,*=,/=)
+j = 4
+j = j + 2  # j holds 6
+j += 2     # also holds 6
+
+# Comparision Operators (==,!=,>,<,>=,<=) - used to compare two operands.
+k = 5
+l = 10
+m = k > l  # m holds False and hence it is boolean type.
+
+
